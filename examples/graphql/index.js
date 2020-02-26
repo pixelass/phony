598fa88c4e385237cccf8139db9a4b2135669618
Gregor Adams <greg@pixelass.com>
2020-02-27 00:51:12 +0100

    chore: adjust scripts

--- examples/graphql/index.js
+++ examples/graphql/index.js
@@ -1,12 +1,10 @@
 const createGraphqlServer = require("@phony/graphql");
-const flush = require("@phony/graphql/dist/flush");
+const flush = require("@phony/graphql/flush");
 const path = require("path");
 const db = require("./db");
 
-async function start() {
+(async function start() {
 	const filePath = path.resolve(__dirname, "db.json");
 	await flush(db, filePath);
-	createGraphqlServer(db, filePath, 3001);
-}
-
-start();
+	createGraphqlServer(db, filePath);
+})();
