4c6c2eb0fa897df64f8909731ec7746ad10cc564
Gregor Adams <greg@pixelass.com>
2020-03-06 00:38:42 +0100

    chore: adjust example

--- examples/graphql/index.js
+++ examples/graphql/index.js
@@ -1,9 +1,10 @@
+const path = require("path");
 const createGraphqlServer = require("@phony/graphql");
 const flush = require("@phony/graphql/flush");
-const db = require("./db");
 const config = require("./phony.config");
 
 (async function start() {
+	const db = require(path.resolve(process.cwd(), config.database));
 	await flush(db, config);
 	createGraphqlServer(db, config);
 })();
