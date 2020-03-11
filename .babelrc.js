1042e5e44701bf7047bec7cad6b34c5204befa46
Gregor Adams <greg@pixelass.com>
2020-03-11 11:51:03 +0100

    chore: configure codecov

--- .babelrc.js
+++ .babelrc.js
@@ -1,13 +1,18 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-typescript"],
-  plugins: [
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-proposal-object-rest-spread",
-    [
-      "babel-plugin-transform-inline-environment-variables",
-      {
-        include: ["NODE_ENV"]
-      }
-    ]
-  ]
+	presets: ["@babel/preset-env", "@babel/preset-typescript"],
+	plugins: [
+		"@babel/plugin-proposal-class-properties",
+		"@babel/plugin-proposal-object-rest-spread",
+		[
+			"babel-plugin-transform-inline-environment-variables",
+			{
+				include: ["NODE_ENV"]
+			}
+		]
+	],
+	env: {
+		test: {
+			plugins: ["babel-plugin-istanbul"]
+		}
+	}
 };
